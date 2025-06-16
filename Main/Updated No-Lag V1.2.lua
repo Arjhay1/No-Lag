174fa6dc8494d145e68500df9e6dc198fcf48e23
Arjhay1 <evonyraid016@gmail.com>
2025-06-17 00:04:27 +0800

    Add files via upload

---
+++ Main/Updated No-Lag V1.2.lua
@@ -0,0 +1,5 @@
+-- Load the first script (No-Lag V1.3) from GitHub
+loadstring(game:HttpGet("https://raw.githubusercontent.com/YOUR_USERNAME/YOUR_REPO/main/UPDATED%20No-Lag%20V1.3.txt"))()
+
+-- Load the second script (LoaderV1.lua) from another GitHub repo
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Loader/LoaderV1.lua"))()
